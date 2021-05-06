7a12a05a9f463b73fc9d77cbb45b9bd59234a9c1
Victor Holguin <vic@vics.pics>
2021-05-06 00:38:06 -0700

    tweak

--- modules/uno/index.js
+++ modules/uno/index.js
@@ -1,7 +1,7 @@
 module.exports = (props, name = 'mono') => {
-	const objCHeck = typeof props === 'object' && !(props instanceof Date) && props !== null;
+	const objCHeck = () => (typeof props === 'object') && !(props instanceof Date) && (props !== null);
 	const { ...rest } = props;
 	let unoString;
-	if (!objCHeck) unoString = props;
+	if (!objCHeck()) unoString = props;
 	return { [name]: unoString, ...rest };
 };
