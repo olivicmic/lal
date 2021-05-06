6b2784771dc23ce8a5c2f76ef0571ffe82944a72
Victor Holguin <vic@vics.pics>
2021-05-06 00:42:40 -0700

    tweak props destructuring bug

--- modules/uno/index.js
+++ modules/uno/index.js
@@ -1,7 +1,6 @@
 module.exports = (props, name = 'mono') => {
 	const objCHeck = () => (typeof props === 'object') && !(props instanceof Date) && (props !== null);
-	const { ...rest } = props;
 	let unoString;
 	if (!objCHeck()) unoString = props;
-	return { [name]: unoString, ...rest };
+	return { [name]: unoString, ...props };
 };
